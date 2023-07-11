fcab7e098465ce57bc3c5df995701f62f23076be
Lastraeus <>
2023-07-11 18:18:45 +0000

    add: bookings app, update allowed hosts

--- codecabin/settings.py
+++ codecabin/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+HOST1 = os.environ.get('CODEANYWHERE_HOST')
+ALLOWED_HOSTS = [HOST1]
 
 
 # Application definition
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookings',
 ]
 
 MIDDLEWARE = [
