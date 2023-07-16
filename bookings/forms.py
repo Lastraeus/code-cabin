c1fb50feca59eaf9d84000a7b7a636a065f759c7
Lastraeus <>
2023-07-16 20:40:33 +0000

    update requirements.txt

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,33 +1,3 @@
-# from django import forms
-# from datetime import datetime
-
-
-# class BookingForm(forms.Form):
-
-#     GUEST_NUM_CHOICES = (
-#         (1, '1'),
-#         (2, '2'),
-#     )
-
-#     checkin_date = forms.DateField(
-#         required=True,
-#         widget=forms.DateInput(
-#             attrs={'type': 'date', 'min': datetime.now().date}
-#         )
-#     )
-#     checkout_date = forms.DateField(
-#         required=True,
-#         widget=forms.DateInput(
-#             attrs={'type': 'date', 'min': datetime.now().date}
-#         )
-#     )
-#     total_guests = forms.IntegerField(
-#         choices=GUEST_NUM_CHOICES, default=1, required=True)
-#     guest_firstname = forms.CharField(max_length=50, required=True)
-#     guest_surname = forms.CharField(max_length=50, required=True)
-#     guest_email = forms.EmailField(required=True)
-
-
 from django.forms import ModelForm
 from .models import Booking
 
